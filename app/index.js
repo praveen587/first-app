4517d44b2a8770b85320bbe879f77dc2e3ce6425
praveen587 <praveen.brcm@gmail.com>
2019-06-15 21:19:49 +0530

    first commit

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class App extends React.Component{
     render(){
         return(
-            <div>Hello World Praveen!!!!!!!!!!!</div>
+            <div>Hello World Praveen!!!!!!!!!!!!</div>
         )
     }
 }
